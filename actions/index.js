9b5c5fdc6c7120d182f90971605430891274fcef
Panithi Aiamsamang <Panithi.Jump3@gmail.com>
2020-11-24 00:16:44 +0700

    Getting movies from server

--- actions/index.js
+++ actions/index.js
@@ -1,38 +1,7 @@
-const MOVIE_DATA = [
-    {
-      id: '1',
-      name: 'The Shawshank Redemption',
-      releaseYear: 1994,
-      description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-      longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus purus a purus porttitor, non tristique leo ullamcorper. Quisque lacinia mi diam, ac accumsan dolor faucibus eget. Suspendisse cursus metus risus, in sollicitudin ex condimentum nec. Suspendisse tempor enim vel porttitor facilisis. Cras vel facilisis urna. Vivamus pharetra nibh laoreet ligula tempus iaculis. Phasellus a sem aliquet, suscipit odio vitae, finibus ligula. Fusce porta velit nisi, vel convallis ante consequat vel. Etiam quis lectus sapien. Duis ut mauris tempus, ultrices ante ut, egestas leo. Praesent at orci volutpat, sollicitudin ex rhoncus, faucibus quam. Phasellus feugiat iaculis orci ac rhoncus. Integer non feugiat nisl, ut hendrerit odio. Cras et faucibus lectus, non tristique neque. Nulla condimentum est nec consectetur molestie.",
-      rating: 4.8,
-      genre: 'drama',
-      image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg',
-      cover: 'https://images.unsplash.com/photo-1585531263717-5ebd8114dac3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-    },
-    {
-      id: '2',
-      name: 'The Dark Knight',
-      releaseYear: 2008,
-      description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-      longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus purus a purus porttitor, non tristique leo ullamcorper. Quisque lacinia mi diam, ac accumsan dolor faucibus eget. Suspendisse cursus metus risus, in sollicitudin ex condimentum nec. Suspendisse tempor enim vel porttitor facilisis. Cras vel facilisis urna. Vivamus pharetra nibh laoreet ligula tempus iaculis. Phasellus a sem aliquet, suscipit odio vitae, finibus ligula. Fusce porta velit nisi, vel convallis ante consequat vel. Etiam quis lectus sapien. Duis ut mauris tempus, ultrices ante ut, egestas leo. Praesent at orci volutpat, sollicitudin ex rhoncus, faucibus quam. Phasellus feugiat iaculis orci ac rhoncus. Integer non feugiat nisl, ut hendrerit odio. Cras et faucibus lectus, non tristique neque. Nulla condimentum est nec consectetur molestie.",
-      rating: 4.7,
-      genre: 'action, crime, drama',
-      image: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600',
-      cover: 'https://images.unsplash.com/photo-1600480505021-e9cfb05527f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1346&q=80'
-    },
-    {
-      id: '3',
-      name: 'Lord of the Rings',
-      releaseYear: 2004,
-      description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-      longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus purus a purus porttitor, non tristique leo ullamcorper. Quisque lacinia mi diam, ac accumsan dolor faucibus eget. Suspendisse cursus metus risus, in sollicitudin ex condimentum nec. Suspendisse tempor enim vel porttitor facilisis. Cras vel facilisis urna. Vivamus pharetra nibh laoreet ligula tempus iaculis. Phasellus a sem aliquet, suscipit odio vitae, finibus ligula. Fusce porta velit nisi, vel convallis ante consequat vel. Etiam quis lectus sapien. Duis ut mauris tempus, ultrices ante ut, egestas leo. Praesent at orci volutpat, sollicitudin ex rhoncus, faucibus quam. Phasellus feugiat iaculis orci ac rhoncus. Integer non feugiat nisl, ut hendrerit odio. Cras et faucibus lectus, non tristique neque. Nulla condimentum est nec consectetur molestie.",
-      rating: 4.9,
-      genre: 'adventure, drama, fantasy',
-      image: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600',
-      cover: 'https://images.unsplash.com/photo-1536972781967-a8aedfb21059?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-    }
-  ]
+import axios from 'axios'
+const BASE_URL = 'http://localhost:3000'
+
+const MOVIE_DATA = []
 
   const CATEGORY_DATA = [
     {id: '1', name: 'drama'},
@@ -63,12 +32,7 @@ const MOVIE_DATA = [
   }
 
   export const getMovies = () =>{
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(MOVIE_DATA)
-          reject('Cannot fetch DATA!')
-        }, 50)
-      })
+    return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
   }
 
 export const getMovieById = (id) => {
