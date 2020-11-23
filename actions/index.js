3fd47580598644f990a518ff7fd14e6e771404fe
Panithi Aiamsamang <Panithi.Jump3@gmail.com>
2020-11-24 00:35:58 +0700

    get movies by id

--- actions/index.js
+++ actions/index.js
@@ -36,11 +36,5 @@ const MOVIE_DATA = []
   }
 
 export const getMovieById = (id) => {
-  return new Promise((resolve, reject) => {
-    const movieIndex = MOVIE_DATA.findIndex(m => m.id === id)
-    const movie = MOVIE_DATA[movieIndex]
-
-    console.log(movie)
-    setTimeout(() => resolve(movie), 50)
-  })
+  return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
 }
