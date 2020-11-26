9dfe81d4a51a3247cbc81ea6c40464a94d760f5c
Panithi Aiamsamang <Panithi.Jump3@gmail.com>
2020-11-26 22:28:37 +0700

    Saving and Delete movie

--- actions/index.js
+++ actions/index.js
@@ -3,38 +3,35 @@ const BASE_URL = 'http://localhost:3000'
 
 const MOVIE_DATA = []
 
-  const CATEGORY_DATA = [
-    {id: '1', name: 'drama'},
-    {id: '2', name: 'action'},
-    {id: '3', name: 'adventure'},
-    {id: '4', name: 'historical'},
-  ]
+const CATEGORY_DATA = [
+  {id: '1', name: 'drama'},
+  {id: '2', name: 'action'},
+  {id: '3', name: 'adventure'},
+  {id: '4', name: 'historical'},
+]
 
-  export const createMovie = (movie) => {
-    return new Promise((resolve, reject) => {
-      // Create ID for movie
-      movie.id = Math.random().toString(36).substr(2, 7)
-      MOVIE_DATA.push(movie)
-      setTimeout(() => {
-        resolve(MOVIE_DATA)
-        // reject('Cannot fetch data!')
-      }, 50)
-    })
-  }
-  
-  export const getCategories = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(CATEGORY_DATA)
-        reject('Cannot fetch DATA!')
-      }, 50)
-    })
-  }
+export const createMovie = (movie) => {
+  movie.id = Math.random().toString(36).substr(2, 5)
+  return axios.post(`${BASE_URL}/api/v1/movies`, movie).then(res => res.data)
+}
 
-  export const getMovies = () =>{
-    return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
-  }
+export const getCategories = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(CATEGORY_DATA)
+      reject('Cannot fetch DATA!')
+    }, 50)
+  })
+}
+
+export const getMovies = () =>{
+  return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
+}
 
 export const getMovieById = (id) => {
   return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
+}
+
+export const deleteMovie = (id) => {
+  return axios.delete(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
 }
